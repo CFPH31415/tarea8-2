da9e8bd3ba89fd7678f715c16c368b038a0982a6
CFPH31415 <121558966+CFPH31415@users.noreply.github.com>
2023-01-02 13:17:10 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { onErrorResumeNext } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto8-2';
-}
+  datos: any = [{
+    nombre: 'Carlos',
+    apellido:'Portillo',
+    edad:25
+  }];
+  registrarDato(nombre:string, apellido:string, edad:string){
+  if(nombre.length && apellido.length && edad.length ){
+    this.datos.push({
+      nombre,apellido,edad
+    });
+  }else{
+   this.datos.push
+    "error"
+   }}
+  }
